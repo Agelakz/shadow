3cb7b5376cf2dedc7b39a499ae8d547379b8c434
Agelakz <38442488+Agelakz@users.noreply.github.com>
2026-03-16 15:30:23 +0800

    Update detail.js

--- api/detail.js
+++ api/detail.js
@@ -1,14 +1,12 @@
 export default async function handler(req, res) {
-    // Menangkap pesanan dari Frontend
     const { type, id, h2h, league, season } = req.query;
     const apiKey = '20178b9693c87fccba1b1cbd4cc44830';
     let url = "";
 
-    // Mengatur jalur sesuai menu yang diklik
     if (type === 'stats') {
         url = `https://v3.football.api-sports.io/fixtures/statistics?fixture=${id}`;
     } else if (type === 'h2h') {
-        url = `https://v3.football.api-sports.io/fixtures/headtohead?h2h=${h2h}&last=5`; // Ambil 5 pertemuan terakhir
+        url = `https://v3.football.api-sports.io/fixtures/headtohead?h2h=${h2h}&last=5`;
     } else if (type === 'standing') {
         url = `https://v3.football.api-sports.io/standings?league=${league}&season=${season}`;
     } else {
@@ -21,8 +19,6 @@ export default async function handler(req, res) {
             headers: { 'x-apisports-key': apiKey }
         });
         const data = await response.json();
-        
-        // Teruskan data asli dari API-Football ke Frontend
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: "Gagal menarik data analisis" });
